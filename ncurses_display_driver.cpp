3b0440a650a94349d1745158da7b8d64ed4b6574
Simon Doppler <dopsi.dev@gmail.com>
2016-03-13 16:50:37 +0100

    Highlight titles

--- ncurses_display_driver.cpp
+++ ncurses_display_driver.cpp
@@ -4,16 +4,32 @@
 
 using namespace std;
 
+#define WIN_TITLE_PAIR 1
+#define TITLE1_PAIR 2
+#define COLOR_NONE -1
+
 NcursesDisplayDriver::NcursesDisplayDriver() :
 	DisplayDriver(), _stdscr(initscr()),
 	_title_window(newwin(3,30,0,0)),
 	_display_window(newwin(60,COLS,2,0)),
 	_display_offset(0) {
+	//Check if all windows have been initialized
 	assert(_title_window);
 	assert(_display_window);
+	assert(has_colors());
+
+	// Begin ncurses mode
 	cbreak();
 	noecho();
 	keypad(stdscr, TRUE);
+
+	// Set up colors
+	start_color();
+	use_default_colors();
+	init_pair(WIN_TITLE_PAIR, COLOR_YELLOW, COLOR_NONE);
+	init_pair(TITLE1_PAIR, COLOR_BLUE, COLOR_NONE);
+
+	// Refresh screen, to enable the windows
 	refresh();
 }
 
@@ -22,7 +38,9 @@ NcursesDisplayDriver::~NcursesDisplayDriver() {
 }
 
 void NcursesDisplayDriver::display(Document * doc) {
+	wattron(_title_window, COLOR_PAIR(WIN_TITLE_PAIR));
 	wprintw(_title_window, "document displayer - title");
+	wattroff(_title_window, COLOR_PAIR(WIN_TITLE_PAIR));
 	wrefresh(_title_window);
 	Paragraph *p(nullptr);
 	LineElement *l(nullptr);
@@ -31,22 +49,24 @@ void NcursesDisplayDriver::display(Document * doc) {
 	for (size_t i(0); i < doc->size(); ++i) {
 		p = (*doc)[i];
 		getyx(_display_window, cursor_y, cursor_x);
-			switch (p->level()) {
-				case Paragraph::Level::Title1:
-				case Paragraph::Level::Title2:
-					if (!is_first) {
-						cursor_y+=2;
-					}
-					cursor_x=9;
-					break;
-				default:
-					if (!is_first) {
-						cursor_y+=2;
-					}
-					cursor_x=0;
-					break;
+		switch (p->level()) {
+			case Paragraph::Level::Title1:
+				wattron(_display_window, A_UNDERLINE);
+			case Paragraph::Level::Title2:
+				wattron(_display_window, COLOR_PAIR(TITLE1_PAIR));
+				if (!is_first) {
+					cursor_y+=2;
+				}
+				cursor_x=9;
+				break;
+			default:
+				if (!is_first) {
+					cursor_y+=2;
+				}
+				cursor_x=0;
+				break;
 	
-			}
+		}
 		if (is_first) {
 			is_first = false;
 		}
@@ -54,6 +74,15 @@ void NcursesDisplayDriver::display(Document * doc) {
 		for (size_t j(0); j < p->size(); ++j) {
 			l = (*p)[j];
 			wprintw(_display_window, (l->content()).c_str());
+		}
+		switch (p->level()) {
+			case Paragraph::Level::Title1:
+				wattroff(_display_window, A_UNDERLINE);
+			case Paragraph::Level::Title2:
+				wattroff(_display_window, COLOR_PAIR(TITLE1_PAIR));
+			default:
+				break;
+	
 		}
 	}
 	wrefresh(_display_window);
