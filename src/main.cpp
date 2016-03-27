c6fef87d906f71ec54798c3e8ee9533cc2cf7df8
Simon Doppler <dopsi.dev@gmail.com>
2016-03-27 17:57:48 +0200

    Make program usable for all locales

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
-#include <iostream>
 #include "configure.hpp"
 #include "parsers/md_parser.hpp"
 #include "display_drivers/ncurses_display_driver.hpp"
 
+#include <iostream>
+#include <locale.h>
+
 using namespace std;
 
 int main(int argc, char ** argv) {
+	setlocale(LC_ALL, "");
 	cout << "mdl v" << MDL_VERSION_STRING << endl;
 	if (argc < 2) {
 		cerr << "mdl: missing at least one argument" << endl;
