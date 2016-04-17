8e67a615d2f06908b0679fa62bdb06fe83854b6e
Simon Doppler <dopsi.dev@gmail.com>
2016-04-17 23:25:47 +0200

    Do not have closing parenthesis in URL's

--- src/parsers/md_parser.cpp
+++ src/parsers/md_parser.cpp
@@ -160,7 +160,7 @@ void MdParser::parse() {
 
 LineElement* MdParser::parse_line(Paragraph *p, const std::string & line) {
 	string clean_line(string_clear_leading(line, " \t#"));
-	regex url_regex("\\[.*\\]\\(.*\\)"), code_regex("`[^`]*`");
+	regex url_regex("\\[.*\\]\\([^)]*\\)"), code_regex("`[^`]*`");
 	smatch match;
 	LineElement *e;
 
