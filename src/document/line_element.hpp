a86a4c750f5698da0afae183fe4f5a54c3787a7f
Simon Doppler <dopsi@member.fsf.org>
2016-07-12 19:34:52 +0200

    Documentation of Paragraph and LineElement

--- src/document/line_element.hpp
+++ src/document/line_element.hpp
@@ -10,10 +10,26 @@
  **/
 class LineElement {
 	protected:
+		/** @brief The contents of the LineElement **/
 		std::string _content;
 	public:
+		/**
+		 * @brief Constructor
+		 *
+		 * @param content The LineElement's content
+		 **/
 		LineElement(const std::string & content);
+
+		/**
+		 * @brief Destructor
+		 **/
 		virtual ~LineElement() {}
+
+		/**
+		 * @brief Get the LineElement's content
+		 *
+		 * @return the LineElement's content (may be an empty string)
+		 **/
 		virtual std::string content(void) const;
 };
 
--- src/document/paragraph.hpp
+++ src/document/paragraph.hpp
@@ -12,27 +12,79 @@
  **/
 class Paragraph {
 	protected:
+		/** @brief List of LineElement in this Paragraph **/
 		std::vector<LineElement*> _elements;
 	public:
+		/**
+		 * @brief Constructor
+		 **/
 		Paragraph();
+		
+		/**
+		 * @brief Conversion constructor
+		 *
+		 * @param p Pointer to the original paragraph
+		 **/
 		Paragraph(Paragraph * p);
+		
+		/**
+		 * @brief Destructor
+		 **/
 		~Paragraph();
+
+		/**
+		 * @brief Append a line element
+		 *
+		 * @param e The pointer to the LineElement to append
+		 **/
 		virtual void append_line_element(LineElement * e);
+
+		/**
+		 * @brief Get the number of LineElement
+		 **/
 		size_t size(void) const;
+
+		/**
+		 * @brief Access a line element
+		 *
+		 * @param index The index of the requested LineElement
+		 * @return A pointer to the LineElement, nullptr 
+		 * if the index does not exist.
+		 **/
 		LineElement * element(size_t index = 0) const;
+
+		/**
+		 * @brief Access a line element
+		 *
+		 * @param index The index of the requested LineElement
+		 * @return A pointer to the LineElement, nullptr 
+		 * if the index does not exist.
+		 **/
 		LineElement * operator[] (size_t index) const;
+
+		/**
+		 * @brief Level of the paragraph
+		 *
+		 * This value is returned by Paragraph::level()
+		 **/
 		enum class Level {
-			Text,
-			Title1,
-			Title2,
-			Title3,
-			Title4,
-			Title5,
-			Title6,
-			UList1,
-			Code,
-			Quote
+			Text /** This is the default level **/,
+			Title1 /** First title **/,
+			Title2 /** Second title **/,
+			Title3 /** Third title **/,
+			Title4 /** Fourth title **/,
+			Title5 /** Fifth title **/,
+			Title6 /** Sixth title **/,
+			UList1 /** Unordered list **/,
+			Code /** Code paragraph **/,
+			Quote /** Quote paragraph **/
 		};
+
+		/**
+		 * @brief Get the Paragraph's Paragraph::Level
+		 *
+		 * @return The level of the Paragraph
+		 **/
 		virtual Level level(void) const = 0;
 };
 
