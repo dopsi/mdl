659209a070449a93960661c01b1c177bb1a9c5b0
Simon Doppler <dopsi@member.fsf.org>
2016-07-26 20:13:56 +0200

    Add DisplayDriver::_standalone

--- libmdl/display_driver.cpp
+++ libmdl/display_driver.cpp
@@ -1,1 +1,6 @@
 #include "includes/display_driver.hpp"
+
+DisplayDriver::DisplayDriver(bool standalone) :
+	_standalone(standalone) {
+	// TDB
+}
--- libmdl/includes/display_driver.hpp
+++ libmdl/includes/display_driver.hpp
@@ -18,8 +18,13 @@ class DisplayDriver {
 		 *
 		 * This constructor does nothing. In the derived class, it 
 		 * can be used to initialize the display system.
+		 *
+		 * @param standalone Set the display driver to output 
+		 * standalone data (see DisplayDriver::_standalone).
 		 **/
-		DisplayDriver() {};
+		DisplayDriver(
+			bool standalone
+			);
 
 		/**
 		 * @brief Destructor
@@ -60,6 +65,18 @@ class DisplayDriver {
 		 * and will not be closed.
 		 **/
 		virtual void display(Document * doc, std::ostream & output) = 0;
+
+	protected:
+		/**
+		 * @brief Wrap the output to make it stand-alone.
+		 *
+		 * This flag is set by default, and **must** be used to determine
+		 * if the output must be standalone.
+		 *
+		 * @example This can set to false to output HTML or LaTex 
+		 * fragments instead of a full file.
+		 **/
+		bool _standalone;
 };
 
 #endif // MDL_DISPLAY_DRIVER_HPP
