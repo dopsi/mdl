51d0d15ddc80c12aef072cd6290a5aaa595fdbb2
Simon Doppler <dopsi@dopsi.ch>
2016-08-25 15:48:00 +0530

    Enable TTY resizing

--- libmdl_tools/includes/tools/tty.hpp
+++ libmdl_tools/includes/tools/tty.hpp
@@ -5,11 +5,15 @@ class TTY {
 	private:
 		unsigned short _lines;
 		unsigned short _columns;
+		void get_winsize(void);
+		void set_winsize(unsigned short c, unsigned short l);
 	public:
 		TTY();
 
 		unsigned short lines(void) const;
+		unsigned short lines(unsigned short l);
 		unsigned short columns(void) const;
+		unsigned short columns(unsigned short c);
 };
 
 #endif // LIBMDL_TOOLS_TTY_HPP
--- libmdl_tools/tty.cpp
+++ libmdl_tools/tty.cpp
@@ -4,10 +4,21 @@
 #include <unistd.h>
 
 TTY::TTY() {
+	get_winsize();
+}
+
+void TTY::get_winsize(void) {
 	struct winsize ws;
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
 	_lines = ws.ws_row;
 	_columns = ws.ws_col;
+}
+
+void TTY::set_winsize(unsigned short c, unsigned short l) {
+	struct winsize ws;
+	_lines = ws.ws_row = l;
+	_columns = ws.ws_col = c;
+	ioctl(STDOUT_FILENO, TIOCSWINSZ, &ws);
 }
 
 unsigned short TTY::lines(void) const {
@@ -15,5 +26,15 @@ unsigned short TTY::lines(void) const {
 }
 
 unsigned short TTY::columns(void) const {
+	return _columns;
+}
+
+unsigned short TTY::lines(unsigned short l) {
+	set_winsize(l, _columns);
+	return _lines;
+}
+
+unsigned short TTY::columns(unsigned short c) {
+	set_winsize(_lines, c);
 	return _columns;
 }
